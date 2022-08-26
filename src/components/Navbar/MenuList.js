8834b3c37e15af314e742d1747cb05923c9ed8a9
Gopal431 <prasadgopal431@gmail.com>
2022-08-26 22:48:30 +0530

    nav_bar_completed

---
+++ src/components/Navbar/MenuList.js
@@ -0,0 +1,22 @@
+export const MenuList = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/features",
+  },
+  {
+    title: "Pricing",
+    url: "/pricing",
+  },
+  {
+    title: "Testimonial",
+    url: "/testimonials",
+  },
+  {
+    title: "Try For Free",
+    url: "/demo",
+  },
+];
